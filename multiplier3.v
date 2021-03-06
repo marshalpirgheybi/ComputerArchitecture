5136df7ae80e811770ba078586c02cb0bdf521e4
mehran-montazer <65732217+mehran-montazer@users.noreply.github.com>
2021-03-06 20:26:40 +0330

    modified mul3

--- multiplier3.v
+++ multiplier3.v
@@ -24,32 +24,28 @@ wire [8:0] adder_output;
 //---------------------------------------------------------
 
 //-------------------------------------- combinational logic
-assign adder_output = Multiplicand + Product[15:8];
+				//checking whether that it's the last partial-product or not
+				//using sign extension to make sure that overflow will not happen
+assign adder_output = (counter[3])? {Product[15],Product[15:8]} - {Multiplicand[7],Multiplicand} : {Multiplicand[7],Multiplicand} + {Product[15],Product[15:8]};
 assign product_write_enable = Product[0];
-assign ready = counter[3];
+assign ready = counter[3] && counter[0];
 //---------------------------------------------------------
 
 //--------------------------------------- sequential Logic
 always @ (posedge clk)
 
-   if(start) begin
-      counter <= 4'h0 ;
-      Product <= {8'h00,B};
-      Multiplicand <= A;
-   end
+    if(start) begin
+        //counter starts from 1 so the conditions in 'combinational logic' part will be easier to understnad
+        counter <= 4'h01 ;
+        Product <= {8'h00,B};
+        Multiplicand <= A;
+    end
 
    else if(! ready) begin
          counter <= counter + 4'b1;
          //using signed shift to make sure that sign extension is properly executed
          Product <= Product >>> 1;
-        if(product_write_enable)
-        //checking whether that it's the last partial-product or not
-        if (counter < 4'b0111) begin
-            Product <= {adder_output,Product[7:1]};            
-        end
-        else begin
-            Product <= ~{adder_output,Product[7:1]};            
-        end
+			if(product_write_enable)begin
+				Product <= {adder_output,Product[7:1]};    
+			end
    end   
-
-endmodule
