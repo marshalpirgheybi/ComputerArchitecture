ce4f69b7eff2a766c104033f24a2a56a49274a47
mehran-montazer <65732217+mehran-montazer@users.noreply.github.com>
2021-03-07 17:14:06 +0330

    dog into ise's soul

--- multiplier3.v
+++ multiplier3.v
@@ -16,17 +16,18 @@ module multiplier3(
 //----------------------------------- register deceleration
 reg [7:0] Multiplicand ;
 reg [3:0]  counter;
+reg carry;
 //-------------------------------------------------------
 
 //------------------------------------- wire deceleration
 wire product_write_enable;
-wire [8:0] adder_output;
+wire [9:0] adder_output;
 //---------------------------------------------------------
 
 //-------------------------------------- combinational logic
 				//checking whether that it's the last partial-product or not
 				//using sign extension to make sure that overflow will not happen
-assign adder_output = (counter[3])? {Product[15],Product[15:8]} - {Multiplicand[7],Multiplicand} : {Multiplicand[7],Multiplicand} + {Product[15],Product[15:8]};
+assign adder_output = (counter[3])? {Product[15],Product[15:8]} - {Multiplicand[7],Multiplicand} : {Multiplicand[7],Multiplicand} + {Product[15],Product[15:8]} ;
 assign product_write_enable = Product[0];
 assign ready = counter[3] && counter[0];
 //---------------------------------------------------------
@@ -34,19 +35,18 @@ assign ready = counter[3] && counter[0];
 //--------------------------------------- sequential Logic
 always @ (posedge clk)
 
-    if(start) begin
-        //counter starts from 1 so the conditions in 'combinational logic' part will be easier to understnad
-        counter <= 4'h01 ;
-        Product <= {8'h00,B};
-        Multiplicand <= A;
-    end
+   if(start) begin
+      counter <= 4'h01 ;
+      Product <= {8'h00,B};
+      Multiplicand <= A;
+   end
 
    else if(! ready) begin
          counter <= counter + 4'b1;
          //using signed shift to make sure that sign extension is properly executed
-         Product <= Product >>> 1;
-         
+         Product <= {Product[15],Product[15:1]};
 			if(product_write_enable)begin
-				Product <= {adder_output,Product[7:1]};    
+				Product <= {adder_output[8:0],Product[7:1]};
 			end
    end   
+endmodule
