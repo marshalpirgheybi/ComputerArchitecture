31078f494c48dca31fe4fbcf3b6ef00b38e6d792
mehran-montazer <65732217+mehran-montazer@users.noreply.github.com>
2021-04-05 00:05:34 +0430

    LAB3 AND LAB4

--- Lab3/Question4.asm
+++ Lab3/Question4.asm
@@ -5,21 +5,25 @@
         jal     function
         j	done
 
-function:       or	    $t0, $a0, $zero
-                or      $v0, $zero, $zero
-                or      $v1, $zero, $zero
-                addi	$s1, $zero, 365
-                addi	$s2, $zero, 30
-                addi	$s3, $zero, 15                
-    year:       blt	    $t0, $s1, month
-                sub     $t0, $t0, $s1
+function:       or	    $t0, $a0, $zero     #putting days in $t0
+                or      $v0, $zero, $zero   #reseting
+                or      $v1, $zero, $zero   #reseting
+                addi	$s1, $zero, 365     #year
+                addi	$s2, $zero, 30      #month
+                addi	$s3, $zero, 15      #half month
+                addi    $s4, $zero, 1       #constant one        
+    year:       slt	    $t1, $t0, $s1       #checking whether days are more than a yaer or not
+                beq     $t1, $s4, month
+                sub	    $t0, $t0, $s1
                 addi	$v0, $v0, 1
                 j	year
-    month:      blt	    $t0, $s2, submonth
+    month:      slt	    $t1, $t0, $s2       #checking whether days are more than a month or not
+                beq     $t1, $s4, submonth
                 sub     $t0, $t0, $s2
                 addi	$v1, $v1, 1
                 j	month
-    submonth:   blt     $t0, $s3, end
+    submonth:   slt     $t1, $t0, $s3       #rounding
+                beq     $t1, $s4, end
                 addi	$v1, $v1, 1
     end:        jr  	$ra
 done:
