73e5935cf408c6a63b87e050775f59b89beffaa0
Amirmohammadmarshalpirgheybi <63447193+Amirmohammadmarshalpirgheybi@users.noreply.github.com>
2021-04-04 23:49:41 +0430

    2 Updated

--- Lab3/Question2.asm
+++ Lab3/Question2.asm
@@ -1,52 +1,21 @@
-.data
-	A:   .word 24
-	B:   .word 32
-	FinalMessage:  .asciiz "The GCD of A and B is: "
-.text 
-.globl main
-main:
-	lw $v0, A
-	lw $v1, B
-	
-	jal GCD
-	
-	add  $s0, $v0, $zero	
-	
-	li $v0, 4
-	la $a0, FinalMessage
-	syscall
-	
-	li $v0, 1
-	add $a0, $s0, $zero
-	syscall 
-	
-	
-	li $v0, 10
-	syscall 
-	
-.globl GCD	
+	jal GCD	
+	add  $s0, $v0, $zero
+	j Done		
 GCD:
 	subu $sp, $sp, 8
 	sw   $ra, ($sp)
 	sw   $s0, 4($sp)
-	
-	
 	#Base case
 	div $v0, $v1
-	move $s0, $v1
+	add $s0, $v1 , $zero
 	mfhi $v1
-	move $v0, $s0
+	add $v0, $s0, $zero
 	beq $v1, 0 , GCDDone
-	
 	jal GCD
-	
-	
-	
-	
-	
 	GCDDone: 
 	lw    $ra, ($sp)
 	lw    $s0, 4($sp)
 	addu  $sp, $sp, 8
 	jr    $ra
+Done:
 	
